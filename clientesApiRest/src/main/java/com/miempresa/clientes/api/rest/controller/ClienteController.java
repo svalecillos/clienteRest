8720f1081cbe0e2fa714a399795cf5e3c1a590e9
svalecillos <svalecillos.93@gmail.com>
2019-09-04 15:45:37 +0200

    subiendo cambios

--- clientesApiRest/src/main/java/com/miempresa/clientes/api/rest/controller/ClienteController.java
+++ clientesApiRest/src/main/java/com/miempresa/clientes/api/rest/controller/ClienteController.java
@@ -1,17 +1,29 @@
 package com.miempresa.clientes.api.rest.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -39,6 +51,8 @@ public class ClienteController {
 	
 	@Autowired
 	private IClienteService clienteService;
+	
+	private final Logger log =  LoggerFactory.getLogger(ClienteController.class);
 	
 	@GetMapping("/clientes")
 	public List<Cliente> index(){
@@ -165,8 +179,24 @@ public class ClienteController {
 	@DeleteMapping("/clientes/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)//Retorna 201 si se ha creado automaticamente 
 	public ResponseEntity<?> delete(@PathVariable Long id) {
+		
 		Map<String, Object> response = new HashMap<>();
+		
 		try {
+			//Borramos la imagen del cliente que se quiera eliminar
+			Cliente cliente = clienteService.findById(id);//Buscamos el cliente
+			String nombreFotoAnterior = cliente.getFoto();
+			
+			//Se pregunta si el nombre de la foto no es null, ademas por seguridad se pregunta si el nombre tiene caracteres
+			if(nombreFotoAnterior !=null && nombreFotoAnterior.length() > 0) {
+				Path rutaFotoAnterior = Paths.get("uploads").resolve(nombreFotoAnterior).toAbsolutePath();//Obtenemos el path absoluto.
+				//Convierte la ruta en un archivo 
+				File archivoFotoAnterior = rutaFotoAnterior.toFile();
+				if(archivoFotoAnterior.exists() && archivoFotoAnterior.canRead()){
+					archivoFotoAnterior.delete();
+				}
+			}
+			
 			clienteService.delete(id);
 		} catch(DataAccessException e) {
 			response.put("mensaje", "Error al eliminar el cliente en la base de datos");
@@ -183,13 +213,72 @@ public class ClienteController {
 		
 		Map<String, Object> response = new HashMap<>();
 		
-		Cliente cliente = clienteService.findById(id);
+		Cliente cliente = clienteService.findById(id);//Buscamos el cliente
 		
 		if(!archivo.isEmpty()){
-			String nombreArchivo = archivo.getOriginalFilename();
+			
+			String nombreArchivo = UUID.randomUUID().toString() + "_" + archivo.getOriginalFilename().replace("  ", "");
+			Path rutaArchivo = Paths.get("uploads").resolve(nombreArchivo).toAbsolutePath();
+			log.info(rutaArchivo.toString());
+			
+			try {
+				//Files.copy(archivo.getInputStream(), rutaArchivo);
+				Files.copy(archivo.getInputStream(), rutaArchivo);
+			} catch (IOException e) {
+				
+				response.put("mensaje", "Error al subir la imagen del cliente " + nombreArchivo);
+				response.put("error", e.getMessage().concat(": ").concat(e.getCause().getMessage()));
+				return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+			}
+			
+			//---------- Si la foto anterior existe, se eliminara para cargar otra foto a partir del usuario -------------//
+			String nombreFotoAnterior = cliente.getFoto();
+			
+			//Se pregunta si el nombre de la foto no es null, ademas por seguridad se pregunta si el nombre tiene caracteres
+			if(nombreFotoAnterior !=null && nombreFotoAnterior.length() > 0) {
+				Path rutaFotoAnterior = Paths.get("uploads").resolve(nombreFotoAnterior).toAbsolutePath();//Obtenemos el path absoluto.
+				//Convierte la ruta en un archivo 
+				File archivoFotoAnterior = rutaFotoAnterior.toFile();
+				if(archivoFotoAnterior.exists() && archivoFotoAnterior.canRead()){
+					archivoFotoAnterior.delete();
+				}
+			}
+			
+			cliente.setFoto(nombreArchivo);
+			
+			clienteService.save(cliente);
+			
+			response.put("cliente", cliente);
+			response.put("mensaje", "Has subido correctamente la imagen: " + nombreArchivo);
 		}
 		
 		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
+	}
+
+	@GetMapping("/uploads/img/{nombreFoto:.+}")//El .+ es una expresion regular que significa que es una extencion de un archivo
+	public ResponseEntity<Resource> verFoto(@PathVariable String nombreFoto){
+		
+		Path rutaArchivo = Paths.get("uploads").resolve(nombreFoto).toAbsolutePath();
+		log.info(rutaArchivo.toString());
+		
+		Resource recurso = null;
+		
+		try {
+			recurso = new UrlResource(rutaArchivo.toUri());
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		
+		//Validamos que el recurso exista y sea leible
+		if(!recurso.exists() && !recurso.isReadable()) {
+			throw new RuntimeException("Error no se pudo cargar la imagen: " + nombreFoto);
+		}
+		
+		//Cabezeras de la respuesta para poder descargarlo.
+		HttpHeaders cabecera = new HttpHeaders();
+		cabecera.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + recurso.getFilename() + "\"");
+		
+		return new ResponseEntity<Resource>(recurso, cabecera, HttpStatus.OK);
 	}
 	
 }
